de89fa6ed5712553737babc94b7e36f1e74be657
JamesZesiger <160616173+JamesZesiger@users.noreply.github.com>
2024-10-28 16:39:16 -0400

    Updated User_Profile.py

--- src/User_Profile.py
+++ src/User_Profile.py
@@ -1,8 +1,19 @@
+from datetime import date
+
+class LengthError(Exception):
+    def __init__(self, *message):
+        self.message = message
+        super().__init__(self.message)
+
 class User:
-    def __init__(self, username, email, password):
-        self.__username = username
-        self.__email = email
-        self.__password = password
+    def __init__(self, username: str, email: str, password: str):
+        if type(username) != str or type(email) != str or type(password) != str: raise TypeError
+        if len(password) < 8: raise LengthError("Password must be at least 8 characters")
+        if len(username) < 3: raise LengthError("Username must be at least 3 characters")
+        if '@' not in email or '.' not in email: raise TypeError("Invalid email")
+        self.__username = str(username)
+        self.__email = str(email)
+        self.__password = str(password)
         self.__expenses = {}
         self.__experience = 0
 
@@ -11,7 +22,8 @@ class User:
         return self.__username
 
     @username.setter
-    def username(self, new_username):
+    def username(self, new_username:str):
+        if len(new_username) < 3: raise LengthError("Username must be at least 3 characters")
         self.__username = new_username
 
     @property
@@ -19,27 +31,30 @@ class User:
         return self.__email
 
     @email.setter
-    def email(self, new_email):
-        self.__email = new_email
+    def email(self, new_email: str):
+        if '@' not in new_email or '.' not in new_email: raise TypeError("Invalid email")
+        self.__email = str(new_email)
 
     @property
     def password(self):
         return self.__password
 
     @password.setter
-    def password(self, new_password):
-        self.__password = new_password
+    def password(self, new_password: str):
+        if len(new_password) < 8: raise LengthError("Password must be at least 8 characters")
+        self.__password = str(new_password)
 
     @property
     def expenses(self):
         return self.__expenses
 
     @expenses.setter
-    def expenses(self, new_expenses):
+    def expenses(self, new_expenses: dict):
         self.__expenses = new_expenses
 
-    def add_expenses(self, date, expense):
-        self.__expenses[date] = expense
+    def add_expenses(self, expense:float):
+        if type(expense)!= float: raise TypeError
+        self.__expenses[date.today()] = expense
 
     def remove_expenses(self, date):
         del self.__expenses[date]
@@ -49,11 +64,11 @@ class User:
         return self.__experience
 
     @experience.setter
-    def experience(self, new_experience):
+    def experience(self, new_experience: int):
         self.__experience = new_experience
 
-    def add_experience(self, experience):
+    def add_experience(self, experience: int):
         self.__experience += experience
 
-    def remove_expenses(self, experience):
+    def remove_experience(self, experience: int):
         self.__experience -= experience
